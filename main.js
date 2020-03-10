06b4ec98ea041d604763791fb48290a0ad46f64f
J-grigri <jurgis.grigorjevs@gmail.com>
2020-03-10 22:03:47 +0700

    local storage not working

--- main.js
+++ main.js
@@ -4,31 +4,49 @@ let inputValue = document.getElementById('todoInput');
 
 let addItem = () => {
     let todoValue = inputValue.value;
-    todoList.push({text:todoValue, isDone: false });
+    todoList.push({ text: todoValue, isDone: false });
     document.getElementById("todoInput").value = "";
+    saveTodos()
     update()
-    }
+}
 
 let update = () => {
     let items = "";
-    for (let i=0; i< todoList.length; i++){
+    for (let i = 0; i < todoList.length; i++) {
         items += `<li style="text-decoration:${todoList[i].isDone ? 'line-through' : ""}" 
         onclick="mark(${i})">${todoList[i].text} 
-        <a>${todoList[i].isDone ? 'Mark undone' : 'Mark done' }</a> 
-        <button id="deleteBtn "href='#' onclick="remove(${i})">Delete</button></li>`;
+        <div class="textColor"><a>${todoList[i].isDone ? 'Mark undone' : 'Mark done'}</a></div><br>
+        <button id="deleteBtn "href='#' onclick="remove(${i})">Delete</button></div></li>`;
     }
     resultArea.innerHTML = items;
+}
 
-    }
-    
-
-
-let remove = (i) => { 
+let remove = (i) => {
     todoList.splice(i, 1)
     update();
+    saveTodos()
 }
 
 let mark = (i) => {
     todoList[i].isDone = !(todoList[i].isDone)
     update()
+    saveTodos()
 }
+//Save data to local storage
+let saveTodos = () => {
+    let str = JSON.stringify(todoList);
+    localStorage.setItem('todoList', str);
+}
+
+// Get data from local storage
+
+let getTodos = () => {
+    let str = localStorage.getItem('todoList');
+    todoList = JSON.parse(str);
+    if (!todoList) {
+        todoList = []
+    }
+}
+
+saveTodos()
+update()
